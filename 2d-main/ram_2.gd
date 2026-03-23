9e69a9078b4f9c07e2138f30a6f143bb5309f3c9
kdowd9 <kdowd9@fordham.edu>
2026-03-22 23:15:04 -0400

    background

--- 2d-main/ram_2.gd
+++ 2d-main/ram_2.gd
@@ -12,8 +12,4 @@ func _process(delta: float) -> void:
 		position.x += speed
 	if Input.is_action_pressed("Move_Left"):
 		position.x -= speed
-	if Input.is_action_pressed("Move_Up"):
-		position.y -= speed
-	if Input.is_action_pressed("Move_Down"):
-		position.y += speed
 	
