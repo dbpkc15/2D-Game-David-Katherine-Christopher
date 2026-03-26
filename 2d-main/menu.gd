f9322c7e62cda37ca407592bc3e4e570cb300113
cgarciarivera3 <cgarciarivera@fordham.edu>
2026-03-26 11:20:15 -0400

    exit button

--- 2d-main/menu.gd
+++ 2d-main/menu.gd
@@ -3,6 +3,10 @@ extends Control
 func _on_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://background.tscn")
 
-
 func _on_button_2_button_down() -> void:
 	get_tree().change_scene_to_file("res://tutorial.tscn")
+
+func _on_exit_button_down() -> void:
+	get_tree().quit()
+
+	
